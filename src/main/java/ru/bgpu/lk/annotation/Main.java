85458f0f5f57cb4cc2a41de04ed81217b60559ea
out <piligrim.a3@gmail.com>
2022-01-19 10:25:25 +0900

    add: Logger

--- src/main/java/ru/bgpu/lk/annotation/Main.java
+++ src/main/java/ru/bgpu/lk/annotation/Main.java
@@ -1,5 +1,17 @@
 package ru.bgpu.lk.annotation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    static {
+        new PropertyWorker().initAnnotation();
+    }
+
+    public static void main(String[] args) {
+        logger.info("Start program.");
+    }
 }
---
+++ src/main/java/ru/bgpu/lk/annotation/PropertyWorker.java
@@ -0,0 +1,13 @@
+package ru.bgpu.lk.annotation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropertyWorker {
+
+    private static Logger logger = LoggerFactory.getLogger(PropertyWorker.class);
+
+    public void initAnnotation() {
+        logger.debug("Init property worker");
+    }
+}
